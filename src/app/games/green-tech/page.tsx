17249c3864d43e23d3bf63ffeec7248057ecec08
Aditya Dash <dashaditya012@gmail.com>
2025-09-11 06:49:25 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/games/green-tech/page.tsx
+++ src/app/games/green-tech/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import {
   Card,
