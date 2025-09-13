3daa44c8ec3eaf0b6e3d69770edad5feb48e522a
Aditya Dash <dashaditya012@gmail.com>
2025-09-13 09:07:07 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         hostname: 'ocean.si.edu',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yale-threesixty.transforms.svdcdn.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
